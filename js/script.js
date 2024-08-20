fea8bbe4924bcb47787e646e4b7855d8d89a4ad6
Black 2.o <black2.o@yahoo.com>
2024-08-20 22:49:39 +0600

    Added After Windows If there any File to Upload

--- js/script.js
+++ js/script.js
@@ -92,14 +92,6 @@ document.addEventListener("DOMContentLoaded", function() {
   
 
 
-
-
-
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
